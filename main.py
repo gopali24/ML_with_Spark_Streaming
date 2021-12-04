c6c23a370d2e15ccb52c89c1cc6bb5de178c2824
Gagan-G-R <gagan2001j@gmail.com>
2021-12-04 23:30:18 +0530

    somethings working but dontknow what

--- main.py
+++ main.py
@@ -3,30 +3,61 @@ from pyspark.streaming import StreamingContext
 from pyspark.sql import SparkSession
 from functools import reduce
 import  pyspark.sql.functions as F
-import json
+import json,math
 import numpy as np
-from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import SGDClassifier,SGDRegressor
 from sklearn.metrics import accuracy_score
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
+from sklearn.metrics import mean_squared_error
+
+
+
 
 import warnings
 warnings.filterwarnings("ignore")
 
 
+
+def vectrize(x_test):
+    cv = CountVectorizer(max_features = 2500)
+    x = cv.fit_transform(x_test).toarray()
+    sc = StandardScaler()
+    x = sc.fit_transform(x)
+    return x
+
+
 def Train(X,Y):
+    cv = CountVectorizer(max_features = 2500)
+    x = cv.fit_transform(X).toarray()
+    y=Y
+    sc = StandardScaler()
+    x = sc.fit_transform(x)
+
+    # print("traning:",x,y)
+
 
     try:
-        classifier.partial_fit(X, Y , classes=list(range(2)))
+        # classifier.partial_fit(X, Y , classes=list(range(2)))
+        classifier.partial_fit(x, y)
 
     except Exception as e: 
         print("error in traning:",e)
 
     try:
-        Y_test_preds = []
-        for k in range(1,test_x.shape[0]): ## Looping through test batches for making predictions
-            Y_preds = classifier.predict(test_x[k])
-            Y_test_preds.extend(Y_preds.tolist())
 
-        print("Test Accuracy      : ",accuracy_score(test_y.reshape(-1), Y_test_preds))
+        
+        y_pred = classifier.predict(test_x)
+        y_pred=list(map(math.ceil,y_pred))
+        print(test_y,y_pred)
+        print("Training Accuracy :", classifier.score(x, y))
+        print("Test Accuracy :", classifier.score(test_x, test_y))
+        print("F1 score :", f1_score(test_y, y_pred))
+        cm = confusion_matrix(test_y, y_pred)
+        print(cm)
+        print("RMSE:",mean_squared_error(test_y, y_pred))
 
     except Exception as e: 
         print("error in accuracy:",e)
@@ -55,9 +86,11 @@ def createDataFrame(rdd):
         df_select_clean.show(truncate=False)
         
         
-        
         train_y = test_df.select(F.collect_list('Label')).first()[0]
-        train_x = test_df.select(F.collect_list('Tweet')).first()[0]
+        train_y=list(map(int,train_y[1:]))
+        train_y=[1 if x==4 else x for x in train_y]
+        train_x = test_df.select(F.collect_list('Tweet')).first()[0][1:]
+
         Train(train_x,train_y)
 
 
@@ -81,18 +114,21 @@ spark.sparkContext.setLogLevel("ERROR")
 
 
 # creating a classifier
-classifier = SGDClassifier()
-
+# classifier = SGDClassifier()
+classifier = SGDRegressor()
 
 
 # getting the test dataset
-test_df = spark.read.csv("my_test.csv")
+test_df = spark.read.csv("test.csv")
 oldColumns = test_df.schema.names
 newColumns = ["Label", "Tweet"]
 test_df= reduce(lambda data, idx: data.withColumnRenamed(oldColumns[idx], newColumns[idx]), range(len(oldColumns)), test_df)
-test_df.show()
+# test_df.show(truncate=False)
 test_y = test_df.select(F.collect_list('Label')).first()[0]
-test_x = test_df.select(F.collect_list('Tweet')).first()[0]
+test_y=list(map(int,test_y[1:]))
+test_y=[1 if x==4 else x for x in test_y]
+test_x = test_df.select(F.collect_list('Tweet')).first()[0][1:]
+test_x=vectrize(test_x)
 
 
 
